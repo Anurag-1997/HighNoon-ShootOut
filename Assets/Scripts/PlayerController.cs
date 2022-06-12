09a5b7ace3454e8e024d9b5f64ac2196c4b35cf4
Anurag-1997 <eng.anurag@gmail.com>
2022-06-12 18:07:44 +0530

    removed old player character and added new character
    removed old character because i didnt like it and added a new spine animated character (Robot) , Programmed idle move and jump animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using UnityEngine.InputSystem;
+
+public class PlayerController: MonoBehaviour
+{
+    [SerializeField] SkeletonAnimation skelelonAnimation;
+    [SerializeField] AnimationReferenceAsset idle,walking,jumping;
+    [SerializeField] string currentState, currentAnimation, previousState;
+    MyInputActions myInputAcitons;
+    Vector2 moveVector;
+    bool Jumped = false;
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] float moveSpeed,jumpSpeed;
+    
+    
+
+    private void Awake()
+    {
+        myInputAcitons = new MyInputActions();
+    }
+    private void Start()
+    {
+        
+        rb2d = GetComponent<Rigidbody2D>();
+        currentState = "Idle";
+        SetCharacterState(currentState);
+    }
+    private void Update()
+    {
+        //Move();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void OnEnable()
+    {
+        myInputAcitons.Player.Enable();
+    }
+    private void OnDisable()
+    {
+        myInputAcitons.Player.Disable();
+    }
+    public void SetAnimation(AnimationReferenceAsset animation,bool loop, float timeScale)
+    {
+        if(animation.name.Equals(currentAnimation))
+        {
+            return;
+        }
+        Spine.TrackEntry animationEntry = skelelonAnimation.state.SetAnimation(0, animation, loop);
+        animationEntry.TimeScale = timeScale;
+        animationEntry.Complete += AnimationEntry_Complete;
+        currentAnimation = animation.name;
+    }
+
+    private void AnimationEntry_Complete(Spine.TrackEntry trackEntry)
+    {
+        if(currentState.Equals("Jumping"))
+        {
+            SetCharacterState(previousState);
+        }
+    }
+
+    public void SetCharacterState(string state)
+    {
+        
+        if(state == "Walking")
+        {
+            SetAnimation(walking, true, 1f);
+        }
+        else if(state == "Jumping")
+        {
+            SetAnimation(jumping, false, 0.5f);
+        }
+        else
+        {
+            SetAnimation(idle, true, 1f);
+        }
+        currentState = state;
+    }
+    public void Move()
+    {
+        moveVector = myInputAcitons.Player.Move.ReadValue<Vector2>();
+        Jumped = myInputAcitons.Player.Jump.WasPressedThisFrame();
+        rb2d.velocity = new Vector2(moveVector.x * moveSpeed, rb2d.velocity.y);
+        if(moveVector.x != 0)
+        {
+            if(!currentState.Equals("Jumping"))
+            {
+                SetCharacterState("Walking");
+            }
+            if (moveVector.x > 0)
+            {
+                transform.localScale = new Vector2(0.25f, 0.25f);
+            }
+            else
+            {
+                transform.localScale = new Vector2(-0.25f, 0.25f);
+            }
+        }
+        else
+        {
+            if (!currentState.Equals("Jumping"))
+            {
+                SetCharacterState("Idle");
+
+            }
+        }
+        if(Jumped)
+        {
+            Jump();
+        }
+        
+    }
+    public void Jump()
+    {
+
+        rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        if(!currentState.Equals("Jumping"))
+        {
+            previousState = currentState;
+        }
+        SetCharacterState("Jumping");
+    }
+}
